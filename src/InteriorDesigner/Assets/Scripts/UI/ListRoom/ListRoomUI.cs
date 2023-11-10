7edd3e3d33e0c0cbce1fb404feda697e26071b14
ShutovKS <schutow.kir@yandex.ru>
2023-11-10 12:22:53 +0300

    Added list room ui

---
+++ src/InteriorDesigner/Assets/Scripts/UI/ListRoom/ListRoomUI.cs
@@ -0,0 +1,32 @@
+using MixedReality.Toolkit.UX;
+using UnityEngine;
+
+public class ListRoomUI : MonoBehaviour
+{
+    public PressableButton Kitchen, Bedroom, LivingRoom, Cabinet, Bathroom, Exit;
+    public GameObject mainMenu;
+    public GameObject styleMenu;
+
+    private void Start()
+    {
+        Kitchen.OnClicked.AddListener(() => OpenStyleMenuRoom(RoomType.Kitchen));
+        Bedroom.OnClicked.AddListener(() => OpenStyleMenuRoom(RoomType.Bedroom));
+        LivingRoom.OnClicked.AddListener(() => OpenStyleMenuRoom(RoomType.LivingRoom));
+        Cabinet.OnClicked.AddListener(() => OpenStyleMenuRoom(RoomType.Cabinet));
+        Bathroom.OnClicked.AddListener(() => OpenStyleMenuRoom(RoomType.Bathroom));
+        Exit.OnClicked.AddListener(OpenMainMenu);
+    }
+
+    private void OpenStyleMenuRoom(RoomType roomType)
+    {
+        styleMenu.GetComponent<StyleMenuUI>().roomType = roomType;
+        styleMenu.SetActive(true);
+        gameObject.SetActive(false);
+    }
+    
+    private void OpenMainMenu()
+    {
+        gameObject.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
