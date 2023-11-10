16ab6223f1b9fdbf7ef3e5e422ea9678863e0a0e
ShutovKS <schutow.kir@yandex.ru>
2023-11-10 23:55:09 +0300

    Added spawn  or no ui

---
+++ src/InteriorDesigner/Assets/Scripts/UI/SpawnOrNo/SpawnOrNoUI.cs
@@ -0,0 +1,36 @@
+using MixedReality.Toolkit.UX;
+using UnityEngine;
+
+public class SpawnOrNoUI : MonoBehaviour
+{
+    public PressableButton yes, no;
+    public GameObject scroolScreen, currentObject;
+    public Transform contentTransform;
+
+    private void Start()
+    {
+        yes.OnClicked.AddListener(Yes);
+        
+        no.OnClicked.AddListener(No);
+    }
+
+    private void No()
+    {
+        Destroy(currentObject);
+        
+        gameObject.SetActive(false);
+        
+        scroolScreen.SetActive(true);
+    }
+
+    private void Yes()
+    {
+        currentObject.transform.SetParent(null);
+        
+        currentObject = null;
+        
+        gameObject.SetActive(false);
+        
+        scroolScreen.SetActive(true);
+    }
+}
