b66ee84dbfca7ddf18eb5826d5b655373fd46ab4
ShutovKS <schutow.kir@yandex.ru>
2023-11-10 09:48:05 +0300

    Added main menu ui

---
+++ src/InteriorDesigner/Assets/Scripts/UI/MainMenu/MainMenuUI.cs
@@ -0,0 +1,26 @@
+using MixedReality.Toolkit.UX;
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public PressableButton startButton;
+    public PressableButton exitButton;
+    public GameObject listRoom;
+    
+    private void Start()
+    {
+        startButton.OnClicked.AddListener(StartGame);
+        exitButton.OnClicked.AddListener(ExitGame);
+    }
+    
+    private void StartGame()
+    {
+        listRoom.SetActive(true);
+        gameObject.SetActive(false);
+    }
+    
+    private void ExitGame()
+    {
+        Application.Quit();
+    }
+}
